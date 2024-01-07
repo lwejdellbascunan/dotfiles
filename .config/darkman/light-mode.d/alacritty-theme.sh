34015d5b97d970777de25ec3939f4f3965f85770
Leyla Wejdell Bascuñan <leylawbascunan@gmail.com>
2024-01-07 20:55:36 +0100

    support toggle for alacritty.toml

--- .config/darkman/light-mode.d/alacritty-theme.sh
+++ .config/darkman/light-mode.d/alacritty-theme.sh
@@ -2,4 +2,8 @@
 
 # alacritty supports changing config realtime so just replace themename
 
+# alacritty.yml
 sed -i -e 's/colors: \*dark_everforest/colors: \*light_everforest/g' ~/.config/alacritty/alacritty.yml
+
+# alacritty.toml
+sed -i -e 's#"~/.config/alacritty/themes/everforest_dark.toml"#"~/.config/alacritty/themes/everforest_light.toml"#g' ~/.config/alacritty/alacritty.toml
